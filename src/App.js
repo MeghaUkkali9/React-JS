789a5a2277df55133eba205ceeff5a1d5841dd64
Megha <meghaukkali3@gmail.com>
2019-12-30 16:40:56 -0500

    Added examples on Functional Components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p> This is my first React example </p>
+
+      <ul>
+        <li>London</li>
+        <li>New York</li>
+        <li>California</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//import App from "./App";
+import MyInfo from "./FuntionalComponents/FunctionalExample";
+
+ReactDOM.render(<MyInfo />, document.getElementById("root"));
+//render instance of that functional component to the browser.
