0668c2943979d88f6f5fae4b387d424c7bd030ee
Megha <meghaukkali3@gmail.com>
2019-12-31 14:18:59 -0500

    Added todo-app1

---
+++ todo-app1/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <input type="checkbox" />
+      <p>Here is a paragraph1</p>
+
+      <input type="checkbox" />
+      <p>Here is a paragraph2</p>
+
+      <input type="checkbox" />
+      <p>Here is a paragraph3</p>
+    </div>
+  );
+}
+
+export default App;
