3c0409f4745e6c5482b14b6be7b3ce88fc11e374
Megha <meghaukkali3@gmail.com>
2019-12-31 13:39:58 -0500

    Added examples on Functional Components

--- src/FuntionalComponents/FunctionalExample.js
+++ src/FuntionalComponents/FunctionalExample.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
 
 function MyInfo() {
   return (
     <div>
+      <Header />
+      <MainContent />
       <h1>Megha Ukkali</h1>
       <p> This is the paragraph about me...</p>
       <p>The top 3 vacations I waould like to visit:</p>
@@ -11,6 +16,7 @@ function MyInfo() {
         <li>London</li>
         <li>Switzerland</li>
       </ol>
+      <Footer />
     </div>
   );
 }
